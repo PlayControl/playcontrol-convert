d20f93e7476a52c8878e37e48431e0207dc9fcda
PhilKunz <github@philkunz.com>
2016-03-25 17:28:23 +0100

    updated npmts

---
+++ test/test.ts
@@ -0,0 +1,12 @@
+/// <reference path="../ts/typings/main.d.ts" />
+/*
+console.log("starting test")
+var playcontrolConvert = require("../dist/index.js");
+playcontrolConvert
+    .from("./test/test.flac")
+    .to("./test/test.m4v")
+    .then(function() {
+        console.log("Conversion successfull");
+});
+*/
+
